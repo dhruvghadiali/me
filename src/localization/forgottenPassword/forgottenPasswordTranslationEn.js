b6554599f90a5d5fb31dcac95924b9e8b2035931
dhruv ghadiali <dhruvghadiali@hotmail.com>
2025-03-08 21:21:08 +0530

    Added localization for forgotten password form

---
+++ src/localization/forgottenPassword/forgottenPasswordTranslationEn.js
@@ -0,0 +1,12 @@
+export const forgottenPasswordFormTranslation = {
+    forgottenPasswordFormHeader: "forgotten password",
+    findAccountButtonLabel: "find account",
+    submitButtonLabel: "submit",
+    verifyAccountLabel: "please, verify your account",
+    otpVerificationMessage: "We have sent an OTP to your registered email address ({{email}}) and phone number ({{email}}). Please check your inbox and enter the OTP to proceed",
+    otpVerificationAlert: "If you do not receive the OTP on your email or phone number, you can try after sometime",
+    findAccountTextFieldLabel: "Username|Email|Phone number",
+    otpTextFieldLabel: "OTP",
+    passwordInputLabel: "password",
+    confirmPasswordInputLabel: "confirm password",
+}
