dc66eb26f880e4b52b06c0cb11c497e783cc5045
dhruv ghadiali <dhruvghadiali@hotmail.com>
2025-11-25 21:16:53 +0530

    Updated auth token expire time

--- src/helpers/authHelpers/index.js
+++ src/helpers/authHelpers/index.js
@@ -6,20 +6,45 @@ export const setAuthData = (user, token) => {
 export const getAuthData = () => {
   const userData = localStorage.getItem("meStudentAuthData");
   const authToken = localStorage.getItem("meStudentAuthToken");
+  let isTokenExpired = false;
   
-  console.log("Auth Token:", authToken, userData);
   if (userData && authToken) {
     try {
-      return {
-        user: JSON.parse(userData),
-        token: authToken
-      };
+      if(authToken){
+        // Decode JWT token to get expiration time
+        const tokenParts = authToken.split('.');
+        if (tokenParts.length === 3) {
+          const payload = JSON.parse(atob(tokenParts[1]));
+          const expirationTime = payload.exp ? new Date(payload.exp * 1000) : null;
+
+          console.log("Token expiration time:", expirationTime);
+          console.log("Current time:", new Date());
+
+          if (expirationTime && new Date() >= expirationTime) {
+            clearAuthData();
+            return null;
+          }else{
+            return {
+              user: JSON.parse(userData),
+              token: authToken
+            }
+          }
+        }else{
+          clearAuthData();
+          return null;
+        }
+      }else{
+        clearAuthData();
+        return null;
+      }
     } catch (error) {
-      console.error("Error parsing auth data:", error);
+      console.error("Error decoding token:", error);
       clearAuthData();
       return null;
     }
   }
+
+  clearAuthData();
   return null;
 };
 
