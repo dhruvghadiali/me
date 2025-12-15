8655c1ae49becd7573a441572f93437efaeb5b73
dhruv ghadiali <dhruvghadiali@hotmail.com>
2025-12-15 21:41:46 +0530

    Updated component for schools list

--- src/components/screens/school/schoolList/index.js
+++ src/components/screens/school/schoolList/index.js
@@ -1,15 +1,26 @@
 import { useSelector } from "react-redux";
+import { useTranslation } from "react-i18next";
+
+import _ from "lodash";
+
+import { Mail, MapPin, Phone } from "lucide-react";
 import { ScrollArea, ScrollBar } from "@MEShadcnComponents/scroll-area";
+import {
+  schoolEmailNotProvided,
+  schoolAddressNotProvided,
+  schoolPhoneNotProvided,
+} from "@MELocalization/languages/en";
 import {
   Card,
   CardContent,
   CardHeader,
   CardTitle,
 } from "@MEShadcnComponents/card";
-import { Mail, MapPin, Phone } from "lucide-react";
 
 const SchoolListComponent = ({ handleCardClick }) => {
   const { schools } = useSelector((state) => state.school);
+  const { t } = useTranslation();
+
   return (
     <div className="h-full">
       <ScrollArea className="h-full">
@@ -23,26 +34,41 @@ const SchoolListComponent = ({ handleCardClick }) => {
           >
             <CardHeader className="p-5">
               <CardTitle className="text-xl font-semibold text-dark">
-                {school.name}
+                {_.upperCase(school.name)}
               </CardTitle>
             </CardHeader>
             <CardContent className="space-y-1 text-sm text-muted-foreground">
               <div className="flex items-start gap-2">
                 <MapPin className="h-3 w-3 mt-1 text-dark" />
                 <span className="flex-1 line-clamp-2">
-                  {school.address || "No address provided"}
+                  {_.upperFirst(
+                    school.address ||
+                      t("schoolAddressNotProvided", {
+                        defaultValue: schoolAddressNotProvided,
+                      })
+                  )}
                 </span>
               </div>
               <div className="flex items-center gap-2">
                 <Phone className="h-3 w-3 text-dark" />
                 <span className="flex-1 line-clamp-1">
-                  {`+91 ${school.phoneNumber}` || "No phone number provided"}
+                  {`+91 ${school.phoneNumber}` ||
+                    _.upperFirst(
+                      t("schoolPhoneNotProvided", {
+                        defaultValue: schoolPhoneNotProvided,
+                      })
+                    )}
                 </span>
               </div>
               <div className="flex items-center gap-2">
                 <Mail className="h-3 w-3 text-dark" />
                 <span className="flex-1 line-clamp-1">
-                  {school.email || "No email provided"}
+                  {school.email ||
+                    _.upperFirst(
+                      t("schoolEmailNotProvided", {
+                        defaultValue: schoolEmailNotProvided,
+                      })
+                    )}
                 </span>
               </div>
             </CardContent>
--- src/localization/school/schoolScreenTranslationEn.js
+++ src/localization/school/schoolScreenTranslationEn.js
@@ -1,9 +1,15 @@
-const schoolsNotFoundHeader = "no schools found";
-const schoolsNotFoundMessage = "it looks like there are no schools available at the moment. please try again later or contact support if you need assistance.";
 const schoolsNotFoundRefreshButton = "refresh";
+const schoolsNotFoundHeader = "no schools found";
+const schoolEmailNotProvided = "no email provided";
+const schoolAddressNotProvided = "no address provided";
+const schoolPhoneNotProvided = "no phone number provided";
+const schoolsNotFoundMessage = `it looks like there are no schools available at the moment. please try again later or contact support if you need assistance.`;
 
 export {
   schoolsNotFoundHeader,
   schoolsNotFoundMessage,
+  schoolPhoneNotProvided,
+  schoolEmailNotProvided,
+  schoolAddressNotProvided,
   schoolsNotFoundRefreshButton,
 };
